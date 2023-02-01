275299439225c7898460f16195e124d32fd16663
Abdurrahman <abdurrahmanms5258@gmail.com>
2023-02-01 15:42:56 +0530

    four variables are changes

--- Large.java
+++ Large.java
@@ -1,17 +1,20 @@
 public class Large {
     public static void main(String[] args) {
-        int n1 = 4, n2= 15, n3= 7;
-        getLargest ( n1,  n2, n3);
+        int n1 = 4, n2= 10, n3= 7, n4=100;
+        getLargest ( n1,  n2, n3, n4);
     }
 
-    private static void getLargest(int n1, int n2, int n3){
-        if(n1>=n2 && n1>=n3){
+    private static void getLargest(int n1, int n2, int n3, int n4){
+        if(n1>=n2 && n1>=n3 && n1>=n4){
             System.out.println(n1 + " is the largest number.");
-        } else if (n2>=n1 && n2>=n3) {
+        } else if (n2>=n1 && n2>=n3 && n2>=n4) {
             System.out.println(n2 + " is the largest number.");
         }
-        else {
+        else if (n3>=n1 && n3>=n2 && n3>=n4) {
             System.out.println(n3 + " is the largest number.");
+        }
+        else {
+            System.out.println(n4 + " is the largest number.");
         }
     }
 }
